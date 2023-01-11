8b5a6f1b3d669532885a53999abe0454a6b26b1b
tutacat <fhwbml@gmail.com>
2023-01-11 02:51:26 +0000

    fix push script

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 x="$(cat version.txt)"
-y="$(echo -e "$x\\n" | python -c "import sys;sys.stdin.write('.'.join(i<2 and s or str(int(s)+1) for i,s in enumerate(input().split('.'))))")"
+y="$(echo -e "$x\\n" | python -c "print(end='.'.join(i<2 and s or str(int(s)+1) for i,s in enumerate(input().split('.'))))")"
 echo "$y" > version.txt
 sed 's/version: "([0-9]{0,3}\.?){1,3}",/version: "'"$y"'",/'
 
