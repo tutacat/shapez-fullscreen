b654b41475c9cadc22b5984eb88fbf1acd38e2ea
tutacat <fhwbml@gmail.com>
2023-01-11 02:55:05 +0000

    Change update stuff

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-x="$(cat version.txt)"
+x="$(< version.txt)"
 y="$(echo -e "$x\\n" | python -c "print(end='.'.join(i<2 and s or str(int(s)+1) for i,s in enumerate(input().split('.'))))")"
 echo "$y" > version.txt
-sed 's/version: "([0-9]{0,3}\.?){1,3}",/version: "'"$y"'",/'
+sed 's/version: "([0-9]{0,3}\.?){1,3}",/version: "'"$y"'",/' -i fullscreen-keybind.js
 
 zip -u fullscreen-keybind.zip fullscreen-keybind.js
 git add fullscreen-keybind.js fullscreen-keybind.zip version.txt
-git commit -m "Update zip to $y"
-git push
+
+if [ "$1" = "push" ]; then
+    git commit -m "Update zip to $y"
+    git push
+fi
